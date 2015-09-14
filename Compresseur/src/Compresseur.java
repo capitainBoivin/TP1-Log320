ffba401192a65ae05bf2c4f157cede4a40b8f4d8
capitainBoivin <catherineboivin.cb@gmail.com>
2015-09-14 12:34:54 -0400

    La liste est un arraylist d'arraylist

--- Compresseur/src/Compresseur.java
+++ Compresseur/src/Compresseur.java
@@ -1,14 +1,18 @@
 import java.io.*;
+import java.lang.reflect.Array;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
+import java.util.Vector;
 
 public class Compresseur {
 
-	HashMap<String, Integer> freqMap = new HashMap<String, Integer>();
-
-	private static File getFile() {
+	//HashMap<String, Integer> freqMap = new HashMap<String, Integer>();
+	ArrayList<ArrayList<Object>> freqTab = new ArrayList<ArrayList<Object>>();
+	/*private static File getFile() {
 		BufferedReader buffReader = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Entrer le lien du texte � d�compresser.");
 		File file=null;
@@ -22,7 +26,7 @@ public class Compresseur {
 			e.printStackTrace();
 		}
 		return file;
-	}
+	}*/
 	
 	public void readFile(File f){
 		BufferedReader br = null;
@@ -34,7 +38,9 @@ public class Compresseur {
 			while ((current = br.readLine()) != null) {
 				verfierTable(current);
 			}
-			System.out.println(freqMap);
+			for(int i=0; i<freqTab.size(); i++) {
+				System.out.println(freqTab.get(i).get(0)+ " " + freqTab.get(i).get(1));
+			}
 
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -47,11 +53,12 @@ public class Compresseur {
 		}	
 	}
 
-	void verfierTable(String mot)
+	/*void verfierTable(String mot)
 	{
 		//Passer a travers les mots
 		for (char lettre: mot.toCharArray())
 		{
+			for 
 			if(freqMap.get(lettre+"")!=null)
 			{
 				freqMap.put(lettre+"",freqMap.get(lettre+"")+1);
@@ -61,14 +68,33 @@ public class Compresseur {
 				freqMap.put(lettre+"",1);
 			}
 		}
-	}
-	//Passer a travers la liste pour les placer en ordre
-	void ordoner()
+	}*/
+	
+	void verfierTable(String mot)
 	{
-		for (String key : freqMap.keySet())
+		//Passer a travers les mots
+		for (char lettre: mot.toCharArray())
 		{
-			;
+			boolean done = false;
+			for (int i=0; i<freqTab.size(); i++) {
+				System.out.println(freqTab.get(i).get(0));
+				if ((freqTab.get(i).get(0)) == (Object)lettre){
+					Integer freq = (Integer)(freqTab.get(i)).get(1);
+					freqTab.get(i).set(1, freq++);
+					done = true;
+				}
+			}
+			if (done == false) {
+					ArrayList<Object> tempo = new ArrayList <Object>();
+					tempo.add(0,lettre);
+					tempo.add(1,1);
+					freqTab.add(tempo);
+			}
 		}
 	}
+	//Passer a travers la liste pour les placer en ordre
+	void ordonner()
+	{
 	
+	}
 }
