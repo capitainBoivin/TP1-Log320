af3a68bf227779244a94488fd44f003106e24171
MAM <ma.majeau@gmail.com>
2015-09-28 23:25:22 -0400

    refactoring et annotation dans le code. Il manque seulement lencodage du header

--- Compresseur/src/Compresseur.java
+++ Compresseur/src/Compresseur.java
@@ -10,28 +10,58 @@ public class Compresseur {
 	private HashMap<Character, Integer> freqMap = new HashMap<Character, Integer>();
 	private HashMap<Character, String> encodedMap = new HashMap<Character, String>();
 	private ArrayList<Node> nodeTab = new ArrayList<Node>();
+	private ArrayList<Node> nodeTabTempo;
 	private int somChar=0;
 	private Node racine;
 	private Node nodeActive;
-	private String texte = "";
+	private String texte;
 	private String ligne;
 	private int paddingBits;
+	private byte [] code;
+	private int indexLecture;
+
 
 	public void readFile(File f){
 		BufferedReader br = null;
-
 		try {
-			br = new BufferedReader(new FileReader(f));
-			while ((ligne = br.readLine()) != null) {
-				verfierTable(ligne);
-				texte += ligne;
+			//trouver l'extension du fichier
+			String fichier = f.getPath().substring(f.getPath().lastIndexOf("\\"));
+			String extension = fichier.substring(fichier.indexOf("."));
+
+			//Verifier le type de fichier
+			if(extension.equals(".txt"))
+			{
+				//Lire le fichier
+				br = new BufferedReader(new FileReader(f));
+				while ((ligne = br.readLine()) != null) {
+					verfierTable(ligne);
+					texte += ligne;
+				}
+
+				quickSort(nodeTab,0,nodeTab.size()-1);
+
+				//Copie du nodeTab pour faire d<autre traitement dessus
+				this.nodeTabTempo= new ArrayList<Node>(nodeTab);
+				//Creation de l'arbre
+				constructHuffmanTree(nodeTab);
+				constructEncodedMap(nodeTab.get(0),encodedMap,new String());
+				encoder();
 			}
-			quickSort(nodeTab,0,nodeTab.size()-1);
-			//Sacrer nodeTab dans un autre variable.
-			constructHuffmanTree(nodeTab);
-			constructEncodedMap(nodeTab.get(0),encodedMap,new String());
-			//Rappeler constrcut HuffmanTree
-			encoder();
+			else if (extension.equals(".txt.huf"))
+			{
+				byte[] contenuEncoder = new byte[(int)f.length()];
+
+				//Pour le fichier en bianire
+				FileInputStream fichierDecode;
+				try {
+					fichierDecode = new FileInputStream(f);
+					fichierDecode.read(contenuEncoder);
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				}
+				//Decode le fichier
+				decoder(contenuEncoder);
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
@@ -43,25 +73,23 @@ public class Compresseur {
 		}	
 	}
 
-	//Faudrait passer le texte et une charactere a la fois et on sait que chacun des charactere exsite dans ils ont ete
-	//denombre, par la suite on passe a travers la larbre a la recherhe et la lettre et ajoute les 0 et 1
 	void encoder()
 	{
-		String fileEncoder = "C:\\Users\\Catherine\\Desktop\\encode.txt.huf";
-		byte [] code;
+		//Fichier hardcoder....cause!!
+		String fileEncoder = "C:\\Users\\Maaj\\Desktop\\encode.txt.huf";
+
 		byte [] headerEncoder;
 
-		System.out.println("hashmap avant de cath");
-		System.out.println(freqMap);
-
+		//Encodage du header
+		headerEncoder=encodeHeader(nodeTabTempo);
+		//Code est le fichier encoder avec le map
 		code=encoderTexte(texte, encodedMap);
-		headerEncoder=encodeHeader(freqMap);
-		decodeHeader(headerEncoder);
-		decoder(code);
 
+		//Ecireture dans le fichier
 		try
 		{
 			BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(fileEncoder));
+			outputStream.write(headerEncoder);
 			outputStream.write(code);
 			outputStream.close();
 		}
@@ -71,9 +99,12 @@ public class Compresseur {
 		}
 	}
 
-	void decoder(byte [] code)
+	void decoder(byte[] code)
 	{
-		String fileDecoder = "C:\\Users\\Catherine\\Desktop\\decode.txt";
+		String fileDecoder = "C:\\Users\\Maaj\\Desktop\\decode.txt";
+
+		//Decodage du header et du code en utilisant le code lu
+		ArrayList headerDecoder=decodeHeader(code);
 		String textePropre=decoderTexte(code);
 
 		try
@@ -109,6 +140,15 @@ public class Compresseur {
 		}
 	}
 
+	//Permet seulement de voir les elements pour une question de debuggage
+	public void verifierChaine(ArrayList liste)
+	{
+		for (Object element:liste)
+		{
+			System.out.println(element);
+		}
+	}
+
 	//Passer a travers la liste pour les placer en ordre
 	private static void quickSort(ArrayList<Node> tableauDeNodes,int debut,int fin)
 	{	//http://java2novice.com/java-sorting-algorithms/quick-sort/
@@ -136,7 +176,8 @@ public class Compresseur {
         if (i < fin)
             quickSort(tableauDeNodes, i, fin);
 	}
-	
+
+	//Changement entre les nodes
 	private static void echanger (ArrayList<Node> tableauDeNodes,int indexGrand,int indexPetit) {
 		Node tempo = tableauDeNodes.get(indexGrand);
 		tableauDeNodes.set(indexGrand, tableauDeNodes.get(indexPetit));
@@ -144,7 +185,8 @@ public class Compresseur {
 	}
 
 	/*--------------------------Arbre---------------------------*/
-	
+
+	//COnstruire l'arbre
 	private void constructHuffmanTree(ArrayList<Node> tableauDeNodes){
 		if (tableauDeNodes.size() == 1){
 			return;
@@ -171,7 +213,7 @@ public class Compresseur {
 	      tableauDeNodes.set(j, newNode);
 	}
 
-	// Début de la référence http://codereview.stackexchange.com/questions/44473/huffman-code-implementation
+	// Dï¿½but de la rï¿½fï¿½rence http://codereview.stackexchange.com/questions/44473/huffman-code-implementation
 	public void constructEncodedMap(Node currentNode, HashMap<Character, String> encodedMap, String encodedString)
 	{
 		if (currentNode.leftChild == null && currentNode.rightChild == null) {
@@ -180,16 +222,20 @@ public class Compresseur {
         }    
 		constructEncodedMap(currentNode.leftChild, encodedMap, encodedString + '0');
 		constructEncodedMap(currentNode.rightChild, encodedMap, encodedString + '1' );
-	} //Fin de la référence
+	} //Fin de la rï¿½fï¿½rence
 
+	//Encoder le texte sans le header
 	private byte[] encoderTexte(String texte, HashMap<Character, String> mapEncoder) {
 		String texteEncodeBinaire = "";
 		StringBuffer sousTexteEncodeBianire = new StringBuffer();
 
+		//Lire chacun des caracteres jusqua la fin du texte
 		for (int i = 0; i != texte.length(); ++i) {
 			sousTexteEncodeBianire.append(mapEncoder.get(texte.charAt(i)));
 		}
+		//Faire une grande chaine et le mettre dans le string
 		texteEncodeBinaire = sousTexteEncodeBianire.toString();
+		//Setting pour meme technique d'insertion
 		ArrayList<String> stringBits = new ArrayList<String>();
 		StringBuffer textePropreBinaire = new StringBuffer();
 
@@ -198,13 +244,16 @@ public class Compresseur {
 
 			if (textePropreBinaire.length() == 8 || i == texteEncodeBinaire.length() - 1) {
 				stringBits.add(textePropreBinaire.toString());
+				//Ajout de bit pour faire des code de meme longeur pour lencodage
 				this.paddingBits = 8 - textePropreBinaire.length();
 				textePropreBinaire.delete(0, textePropreBinaire.length());
 			}
 		}
 
+		//Donner le size egal a notre texte(sauver de lespace)
 		byte[] texteEcnodeFinal = new byte[stringBits.size()];
 
+		//Encodage final encoder
 		for (int i = 0; i != stringBits.size(); ++i) {
 			texteEcnodeFinal[i] = (byte)Integer.parseInt(stringBits.get(i), 2);
 		}
@@ -212,31 +261,32 @@ public class Compresseur {
 		return texteEcnodeFinal;
 	}
 
-	//Decoder de bytes en binaire
+	//Decoder de bytes en texte
 	private String decoderTexte(byte[] fichierEncoder) {
+		//meme principe de recipient avant de mettre dans le string directement
 		StringBuilder texteBinaire = new StringBuilder();
 		StringBuilder textePropre = new StringBuilder();
-		String padString = "00000000";
+		String chainePadding = "00000000";
 		String texteFinalEncoder = "";
 		String texteFinalBinaire = "";
 
 		//decoder et remettre en binaire
-		for (int i =0; i != fichierEncoder.length; ++i) {
+		for (int i = this.indexLecture; i != fichierEncoder.length; i++) {
 			texteFinalEncoder = Integer.toBinaryString(fichierEncoder[i] & 0xFF);
+			//Verification de la longeur des string de byte pour decoder, sinon ajout avec le padding
 			if (i != fichierEncoder.length - 1) {
-				texteBinaire.append(padString.substring(texteFinalEncoder.length()));
+				texteBinaire.append(chainePadding.substring(texteFinalEncoder.length()));
 			} else {
-				texteBinaire.append(padString.substring(texteFinalEncoder.length()+this.paddingBits));
+				texteBinaire.append(chainePadding.substring(texteFinalEncoder.length()+this.paddingBits));
 			}
 			texteBinaire.append(texteFinalEncoder);
 		}
-
 		texteFinalBinaire = texteBinaire.toString();
 
+		//Mettre la node active au debut de larbre
 		nodeActive=nodeTab.get(0);
 
-
-		for (int i = 0; i != texteFinalBinaire.length(); ++i)
+		for (int i = 0; i != texteFinalBinaire.length(); i++)
 		{
 			//Aller a gauche si 0, aller a droite si 1
 			if (texteFinalBinaire.charAt(i) == '0') {
@@ -247,7 +297,6 @@ public class Compresseur {
 					textePropre.append(nodeActive.clef);
 					nodeActive=nodeTab.get(0);
 				}
-
 			} else if (texteFinalBinaire.charAt(i) == '1') {
 				if (nodeActive.rightChild instanceof Node) {
 					nodeActive = nodeActive.rightChild;
@@ -263,17 +312,31 @@ public class Compresseur {
 
 
 	//Ecnoder le header avec la table de frequence
-	private byte[] encodeHeader(HashMap<Character, Integer> freqMap) {
+	private byte[] encodeHeader(ArrayList<Node> nodeTab) {
 		//Se faire un array de 256 a 0 pour se faire un array assez gros et controler
 		int[] entierHeader = new int[256];
 		Arrays.fill(entierHeader, 0);
 
 		//Aller cherhcer chacun des caracteres dans notre table de frequence
-		for (Map.Entry<Character, Integer> entry : freqMap.entrySet()) {
+		/*for (Map.Entry<Character, Integer> entry : freqMap.entrySet()) {
 			entierHeader[(byte)(char)entry.getKey()] = entry.getValue();
+		}*/
+
+		for (Node entrer : nodeTab) {
+
+			entierHeader[(byte)(char)(entrer.clef)]=entrer.freq;
+			System.out.println(entrer.clef);
+			System.out.println(entrer.freq);
 		}
 
-		//Variables pour travailler avec et qui servent de tempo
+
+
+		System.out.println("entier header");
+		System.out.println(entierHeader);
+
+
+
+		//M[eme principe de recipient encore avec la padding de bits pou l<ajustement en 16 bit
 		String charDesBytes = "";
 		StringBuilder sousCharDesBytes = new StringBuilder();
 		String bufferString = "00000000000000000000000000000000";
@@ -291,30 +354,31 @@ public class Compresseur {
 				sousFredMap.append(frequence);
 			}
 		}
-
 		charDesBytes = sousCharDesBytes.append(sousFredMap.toString()).toString();
 
-
+		//Liste des chaine de bits
 		ArrayList<String> stringBits = new ArrayList<String>();
 		StringBuffer sousTexteEncoder = new StringBuffer();
 
 		//Faire des compressions en 8 bit pour encoder le header
-		for (int i = 0; i != charDesBytes.length(); ++i) {
+		for (int i = 0; i != charDesBytes.length(); i++) {
 			sousTexteEncoder.append(charDesBytes.charAt(i));
-
+			//Verification de la longeur pour un bon encodage
 			if (sousTexteEncoder.length() == 8 || i == charDesBytes.length() - 1) {
 				stringBits.add(sousTexteEncoder.toString());
 				sousTexteEncoder.delete(0, sousTexteEncoder.length());
 			}
 		}
 
+		//La longeur pour pouvoir boucler jusqu'a la fin
 		byte[] bytesDepart = new byte[stringBits.size() + 1];
 
-		for (int i = 0; i != stringBits.size(); ++i) {
+		//Ajouter les nombres en byte de base deux dans le chaine de byte
+		for (int i = 0; i != stringBits.size(); i++) {
 			bytesDepart[i + 1] = (byte)Integer.parseInt(stringBits.get(i), 2);
 		}
 
-		//Ajout des bit de pading pour avoir une grandeur fixe et pour permettre de bien decoder
+		//Mettre notre padding dans un indice pour le reutiliser
 		bytesDepart[0] = (byte)this.paddingBits;
 
 		System.out.println("header encode");
@@ -322,21 +386,23 @@ public class Compresseur {
 		return bytesDepart;
 	}
 
+	//Decode le header dans le fichier
+	private ArrayList<Node> decodeHeader(byte[] texteEncoder) {
+		//HashMap<Character, Integer> freqMap = new HashMap<Character, Integer>();
+		//Array pour refaire un tableau de fr/quence pour refaire l'arbre
+		ArrayList<Node> nodeFreq=new ArrayList<Node>();
+		//Aller chercher le type de padding mis dans le texte pour l'utiliser
+		this.paddingBits = texteEncoder[0] & 0xFF;
 
-
-
-	private HashMap<Character, Integer> decodeHeader(byte[] arbreEncoder) {
-		HashMap<Character, Integer> freqMap = new HashMap<Character, Integer>();
-		this.paddingBits = arbreEncoder[0] & 0xFF;
-
+		//Principe de recipient again
 		StringBuilder sousHeaderEncoder = new StringBuilder();
-		String padString = "00000000";
+		String chainePadding = "00000000";
 		String headerEncoderTempo = "";
 		String headerEncoder = "";
 
-		for (int i = 1; i != 33; ++i) {
-			headerEncoderTempo = Integer.toBinaryString(arbreEncoder[i] & 0xFF);
-			sousHeaderEncoder.append(padString.substring(headerEncoderTempo.length()));
+		for (int i = 1; i != 33; i++) {
+			headerEncoderTempo = Integer.toBinaryString(texteEncoder[i] & 0xFF);
+			sousHeaderEncoder.append(chainePadding.substring(headerEncoderTempo.length()));
 			sousHeaderEncoder.append(headerEncoderTempo);
 		}
 
@@ -347,33 +413,55 @@ public class Compresseur {
 		//Remplissage de la variable avec du vide
 		Arrays.fill(frequence, 0);
 
+		//Mettre l'index de depart comme pour la boucle
 		int index =33;
 
-
-		for (int i = 0; i != headerEncoder.length(); ++i) {
+		//Par chacun des caractres dans le header encoder
+		for (int i = 0; i != headerEncoder.length(); i++) {
 			if (headerEncoder.charAt(i) == '1') {
+				//Principe de recipeint encore
 				StringBuilder sousFrenquenceTempo = new StringBuilder();
 				String frenquenceTempo = "";
-				frenquenceTempo = Integer.toBinaryString(arbreEncoder[index] & 0xFF);
-				sousFrenquenceTempo.append(padString.substring(frenquenceTempo.length()));
+
+				frenquenceTempo = Integer.toBinaryString(texteEncoder[index] & 0xFF);
+				sousFrenquenceTempo.append(chainePadding.substring(frenquenceTempo.length()));
 				sousFrenquenceTempo.append(frenquenceTempo);
-				frenquenceTempo = Integer.toBinaryString(arbreEncoder[index+1] & 0xFF);
-				sousFrenquenceTempo.append(padString.substring(frenquenceTempo.length()));
+
+				frenquenceTempo = Integer.toBinaryString(texteEncoder[index+1] & 0xFF);
+				sousFrenquenceTempo.append(chainePadding.substring(frenquenceTempo.length()));
 				sousFrenquenceTempo.append(frenquenceTempo);
-				frenquenceTempo = Integer.toBinaryString(arbreEncoder[index+2] & 0xFF);
-				sousFrenquenceTempo.append(padString.substring(frenquenceTempo.length()));
+
+				frenquenceTempo = Integer.toBinaryString(texteEncoder[index+2] & 0xFF);
+				sousFrenquenceTempo.append(chainePadding.substring(frenquenceTempo.length()));
 				sousFrenquenceTempo.append(frenquenceTempo);
-				frenquenceTempo = Integer.toBinaryString(arbreEncoder[index+3] & 0xFF);
-				sousFrenquenceTempo.append(padString.substring(frenquenceTempo.length()));
+
+				frenquenceTempo = Integer.toBinaryString(texteEncoder[index+3] & 0xFF);
+				sousFrenquenceTempo.append(chainePadding.substring(frenquenceTempo.length()));
 				sousFrenquenceTempo.append(frenquenceTempo);
+
 				index += 4;
 
-				freqMap.put((char)i, Integer.parseInt(sousFrenquenceTempo.toString(), 2));
+				//On obtient le charactere et la frequence,pour chacun on fait des node et on le retransforme en decimal
+				nodeFreq.add(new Node((char)i, Integer.parseInt(sousFrenquenceTempo.toString(), 2)));
 			}
 		}
-		System.out.println("frequence decoder");
-		System.out.println(freqMap);
-		return freqMap;
-	}
 
+		//Pour ne pas relire le header en decodant le texte, on me l'index a un point a commencer
+		this.indexLecture = index;
+
+		/*MapValueComparator mapValueComparator = new MapValueComparator(frequencyTable);
+		TreeMap<Character, Integer> sortedFrequencyTable = new TreeMap<Character, Integer>(mapValueComparator);
+		sortedFrequencyTable.putAll(frequencyTable);*/
+
+		//Refaire les etapes pour bien faire l'arbre
+		quickSort(nodeFreq, 0, nodeFreq.size() - 1);
+		encodedMap=new HashMap<Character, String>();
+		constructHuffmanTree(nodeFreq);
+		constructEncodedMap(nodeFreq.get(0),encodedMap,new String());
+
+		System.out.println("frequence decoder");
+		System.out.println(nodeFreq);
+		nodeTab= new ArrayList<Node>(nodeFreq);
+		return nodeFreq;
+	}
 }
